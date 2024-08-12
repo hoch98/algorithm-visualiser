77cc03febe26595c8115d4778da7d3cbfa364da6
Craft Yun <craftyun83@gmail.com>
2024-08-12 14:10:40 +0800

    added creating and removing edges

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var canvas = document.getElementById("canvas")
-var nodeSlider = document.getElementById("nodes");
-var numOfNodes = 5;
+var canvas = document.getElementById("canvas");
+var edgeSettings = document.getElementById("edges");
 var nodes = [];
 var edges = []
 var dragging = false;
@@ -11,15 +10,29 @@ var lineCanvas = document.querySelector('canvas');
 var context = lineCanvas.getContext('2d');
 fitToContainer();
 
-function deselectNode() {
+function selectNode(node) {
+  deselectNodes();
+  selectedNode = node;
+  selectedNode.classList.add("selected");
+  deselectEdges();
+}
+
+function selectEdge(edge) {
+  selectedEdge = edge;
+  context.lineWidth = 2;
+  deselectNodes();
+  drawEdges();
+}
+
+function deselectNodes() {
   if (selectedNode) {
     selectedNode.classList.remove("selected");
     selectedNode = undefined;
   }
 }
 
-function deselectEdge() {
-  if (selectedNode) {
+function deselectEdges() {
+  if (selectedEdge) {
     selectedEdge = undefined;
     drawEdges();
   }
@@ -32,6 +45,23 @@ function fitToContainer(){
 }
 
 window.onresize = fitToContainer
+
+function updateEdges() {
+  edges = []
+  for (var i = 0; i < edgeSettings.children.length; i++) {
+    var child = edgeSettings.children[i];
+    var inputs = child.querySelectorAll("input")
+    
+    if (inputs[0].value == "") {
+      inputs[0].value = "0"
+    } if (inputs[1].value == "") {
+      inputs[1].value = "0"
+    }
+    
+    edges.push([parseInt(inputs[0].value), parseInt(inputs[1].value)])
+  }
+  drawEdges();
+}
 
 function elementsFromPoint(x, y) { // pointer-events: none;
   const elements = nodes;
@@ -106,7 +136,7 @@ function drawEdges() {
 }
 
 
-function generateNodes(n) {
+function generateNodes(n, nodeEdges=[]) {
   nodes = []
   edges = []
   canvas.innerHTML = ""
@@ -116,24 +146,21 @@ function generateNodes(n) {
     canvas.appendChild(node);
     node.onpointerdown = (event) => {
       dragging = elementsFromPoint(event.clientX, event.clientY)[0];
-      deselectNode();
+
+      deselectNodes();
       
       if (selectedNode !== dragging) {
-        selectedNode = dragging;
-        selectedNode.classList.add("selected");
-        deselectEdge();
+        selectNode(dragging);
       }
     }
-    for (var k = 0; k < nodes.length-1; k++) {
-      var edge = [i, k];
-      edges.push(edge);
-    }
+    edges = nodeEdges
   }
   drawEdges();
 }
 
 lineCanvas.onpointerdown = (event) => {
-  deselectNode();
+
+  deselectNodes();
 
   var mouse = {
     x: event.clientX,
@@ -151,10 +178,7 @@ lineCanvas.onpointerdown = (event) => {
     context.lineTo(bound2.x+25, bound2.y+25);
 
     if (context.isPointInStroke(mouse.x, mouse.y)) {
-      selectedEdge = edge
-      deselectNode();
-      context.lineWidth = 2
-      drawEdges();
+      selectEdge(edge);
       break;
     }
   }
@@ -171,10 +195,60 @@ window.onpointerup = () => {
   dragging = undefined;
 }
 
-nodeSlider.oninput = () => {
-  numOfNodes = document.getElementById("nodes").value;
-  document.getElementById("numOfNodes").textContent = numOfNodes;
-  generateNodes(numOfNodes);
+edgeSettings.oninput = updateEdges
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-generateNodes(3);
+function createEdgeContainer() {
+  var edgeInputContainer = document.createElement("div");
+  edgeInputContainer.classList.add("edgeInputContainer");
+
+  var horLine = document.createElement("div");
+  horLine.classList.add("hor-line");
+
+  var nodeInput = document.createElement("div");
+  nodeInput.classList.add("node");
+  
+  var nodeIdInput = document.createElement("input");
+  nodeIdInput.type = "number";
+  nodeIdInput.classList.add("nodeIdInput");
+  nodeIdInput.setAttribute("min", 0);
+  nodeIdInput.setAttribute("max", nodes.length-1);
+  nodeIdInput.value = 0
+  nodeIdInput.required = true
+
+  nodeInput.appendChild(nodeIdInput)
+
+  var nodeInput2 = nodeInput.cloneNode(true);
+  nodeInput2.value = 0
+
+  var deleteButton = document.createElement("button");
+  deleteButton.classList.add("deleteButton")
+  deleteButton.innerText = "X"
+  deleteButton.style.display = "inline-flex"
+
+  deleteButton.onclick = () => {
+    deleteButton.parentElement.parentElement.removeChild(deleteButton.parentElement);
+    updateEdges();
+  }
+
+  edgeInputContainer.appendChild(nodeInput);
+  edgeInputContainer.appendChild(horLine);
+  edgeInputContainer.appendChild(nodeInput2);
+
+  edgeInputContainer.appendChild(deleteButton)
+
+  edgeSettings.appendChild(edgeInputContainer)
+}
+
+document.getElementById("createEdgeContainer").onclick = createEdgeContainer;
+
+async function dfs() {
+  
+}
+
+document.getElementById("loopSwitch").onclick = dfs
+
+generateNodes(3, []);
